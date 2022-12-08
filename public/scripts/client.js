a2a78f5ddf8eafe6bce724c1920119d4efd2fc57
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-12-07 23:26:24 -0500

    updated erorrs to use jquery slide down and up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,62 +1,46 @@
 // hard code data for examples
-$(document).ready(function() {
+$(document).ready(function () {
 
   const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
+
   ]
 
-  
+  const renderTweets = function (tweets) {
 
-  const renderTweets = function(tweets) {
+    $('.tweet-container').empty();
 
     for (let tweet of tweets) {
-  
+
       let addedTweet = createTweetElement(tweet);
-  
+
       $('.tweet-container').prepend(addedTweet);
     }
   }
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
-const createTweetElement = function(tweet) {
-  // console.log(tweet);
-  let $tweet = `
+  const createTweetElement = function (tweet) {
+    // console.log(tweet);
+    let $tweet = `
 
     <article class="user">
     <div class="tweet-holder">
       <div class="tweet-header">
         <div>
-          <img src="${tweet.user.avatars}">${tweet.user.name}
+          <img src="${escape(tweet.user.avatars)}">${escape(tweet.user.name)}
           <!-- <h4>Newton</h4> -->
         </div>
-        <h4>${tweet.user.handle}</h4>
+        <h4>${escape(tweet.user.handle)}</h4>
       </div>
-      <p class="tweeted-words"> ${tweet.content.text}</p>
+      <p class="tweeted-words"> ${escape(tweet.content.text)}</p>
     
     <footer>
-      <p>${tweet.created_at}</p>
+      <p class="timeago">${escape(timeago.format(tweet.created_at))}</p>
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -66,14 +50,64 @@ const createTweetElement = function(tweet) {
     </footer>
   </article>
   `;
-  
-  return $tweet;
+
+    return $tweet;
+  }
+
+
+  renderTweets(data);
+
+
+  const noChars = () => {
+    let err = ' 🔺 Tweet must be atleast 1 characters! 🔺 ';
+    $('.error-message').text(err);
+
+  }
+
+  const overChars = () => {
+    let err = ' 🔺 Tweet must be less than 140 characters! 🔺 ';
+    $('.error-message').text(err);
   }
 
 
 
+  $('#main-form').submit((event) => {
+    event.preventDefault();
+    
+
+    $('.error-message').slideUp();
+
+    const serializedData = $('#main-form').serialize();
   
-renderTweets(data);
+    // getting length of text box for tweets
+    const textBoxChars = $('.borderless-box').val().length;
+
+    //applying alert based on situation
+    if (textBoxChars === 0) {
+      $('.error-message').slideDown('slow',noChars());
+    } else if (textBoxChars > 140) {
+      $('.error-message').slideDown('slow', overChars());
+    } else {
+
+    $.ajax('/tweets', { method: 'POST', data: serializedData })
+      .then(function () {
+        loadTweets();
+      });
+    }
+  });
+
+
+  
+
+
+  const loadTweets = () => {
+    $.get('/tweets', function (tweets) {
+      renderTweets(tweets);
+    }); 
+  };
+
+
+  loadTweets();
 
 });
   // const $tweet = createTweetElement(tweetData);
