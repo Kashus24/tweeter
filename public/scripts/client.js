7736bd64c47757814a5dfa5fdf29d8fdaffc6f89
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-12-08 22:13:08 -0500

    cleaned code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,24 @@
 // hard code data for examples
 $(document).ready(function () {
 
-  const data = [
+  // data array to hold all tweets
+  const data = [];
 
-  ]
 
   const renderTweets = function (tweets) {
-
     $('.tweet-container').empty();
 
     for (let tweet of tweets) {
-
       let addedTweet = createTweetElement(tweet);
-
       $('.tweet-container').prepend(addedTweet);
-    }
-  }
+    };
 
+  //reset the text box to empty upon successful tweet
+    document.getElementById('tweet-text').value='';
+  };
+
+
+  // escape function to prevent potential outside user manipulation
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
@@ -25,9 +27,7 @@ $(document).ready(function () {
 
 
   const createTweetElement = function (tweet) {
-    // console.log(tweet);
     let $tweet = `
-
     <article class="user">
     <div class="tweet-holder">
       <div class="tweet-header">
@@ -50,9 +50,8 @@ $(document).ready(function () {
     </footer>
   </article>
   `;
-
     return $tweet;
-  }
+  };
 
 
   renderTweets(data);
@@ -62,21 +61,18 @@ $(document).ready(function () {
     let err = ' 🔺 Tweet must be atleast 1 characters! 🔺 ';
     $('.error-message').text(err);
 
-  }
+  };
 
   const overChars = () => {
     let err = ' 🔺 Tweet must be less than 140 characters! 🔺 ';
     $('.error-message').text(err);
-  }
-
+  };
 
 
   $('#main-form').submit((event) => {
     event.preventDefault();
-    
-
     $('.error-message').slideUp();
-
+    
     const serializedData = $('#main-form').serialize();
   
     // getting length of text box for tweets
@@ -92,16 +88,16 @@ $(document).ready(function () {
     $.ajax('/tweets', { method: 'POST', data: serializedData })
       .then(function () {
         loadTweets();
+        
       });
-    }
+    };
+
+  
   });
 
 
-  
-
-
   const loadTweets = () => {
-    $.get('/tweets', function (tweets) {
+     $.get('/tweets', function (tweets) {
       renderTweets(tweets);
     }); 
   };
@@ -110,10 +106,4 @@ $(document).ready(function () {
   loadTweets();
 
 });
-  // const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-//   console.log($tweet); 
-//   $('#tweet-container').append($tweet);
-
-// })
+ 
